bcc64d4d1b890258f8e1d6c9b66ea9877b227cd0
Vineet Nair <vineetnr1@gmail.com>
2024-09-27 22:12:46 +0530

    added ohayas drivers to kernel

---
+++ drivers/ohayas/anti_rasp/kony.h
@@ -0,0 +1,8 @@
+#ifndef _OHY_KONY_IMPLEMENTATION_H
+#define _OHY_KONY_IMPLEMENTATION_H
+
+#include "../arm64/ohyexports.h"
+#include "../arm64/ohyinterceptor.h"
+#include "../ohylog.h"
+
+#endif
---
+++ drivers/ohayas/anti_rasp/promonshield.h
@@ -0,0 +1,7 @@
+#ifndef _OHY_PROMON_IMPLEMENTATION_H
+#define _OHY_PROMON_IMPLEMENTATION_H
+
+#include "../arm64/ohyexports.h"
+#include "../arm64/ohyinterceptor.h"
+
+#endif
